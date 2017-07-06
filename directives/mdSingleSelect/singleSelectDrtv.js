fc46a01f85396fc72976648c37785ccbfcb07ad4
dcoughlin <dcelemeno@gmail.com>
2017-07-06 08:10:18 -0400

    add unique ids to single select box

--- directives/mdSingleSelect/singleSelectDrtv.js
+++ directives/mdSingleSelect/singleSelectDrtv.js
@@ -5,14 +5,15 @@ angular.module('retrieve').directive('singleSelect', function() {
 		templateUrl: 'directives/mdSingleSelect/singleSelect.html',
 		scope: { 
 			options: '=', 
-			selected: "=" 
+			selected: "=",
+			currentid: "=" 
 		},
 		controllerAs: 'select',
 		controller: ['$scope', function($scope){
 			/*  PLANS:
 			 *	options:
 			 *		1. to open only down
-			 *		2. to make based on id, so could have multiple per page
+			 *		done to make based on id, so could have multiple per page
 			 *		3. minimize the amount of jquery
 			*/
 			var _this = this;
@@ -43,117 +44,87 @@ angular.module('retrieve').directive('singleSelect', function() {
 
 			//overall function initially called
 			$(function() {
-			  var closeSelectTimeout;
+				var closeSelectTimeout;
 			
-			  function hideMaterialList(parent) {
-			    parent.css({
-			      'overflow': 'hidden'
-			    }).removeClass('isOpen');
-			    clearTimeout(closeSelectTimeout);
-			    closeSelectTimeout = setTimeout(function() {
-			      parent.parent().css({
-			        'z-index': 0
-			      });
-			    }, 200);
-			  }
+				//hides the list
+				function hideMaterialList(parent) {
+					parent.css({'overflow': 'hidden'}).removeClass('isOpen');
+					clearTimeout(closeSelectTimeout);
+					closeSelectTimeout = setTimeout(function() {
+						parent.parent().css({'z-index': 0});
+					}, 200);
+				}
 
-			  //opening animation
-			  $(document.body).on('mousedown', '.materialBtn, .select li', function(event) {
-			    if (parseFloat($(this).css('opacity')) > 0 && $(document).width() >= 1008) {
-			      var maxWidthHeight = Math.max($(this).width(), $(this).height());
-			      if ($(this).find("b.drop").length == 0 || $(this).find("b.drop").css('opacity') != 1) {
-			        drop = $('<b class="drop" style="width:' + maxWidthHeight + 'px;height:' + maxWidthHeight + 'px;"></b>').prependTo(this);
-			      } else {
-			        $(this).find("b.drop").each(function() {
-			          if ($(this).css('opacity') == 1) {
-			            drop = $(this).removeClass("animate");
-			            return;
-			          }
-			        })
-			      }
-			      x = event.pageX - drop.width() / 2 - $(this).offset().left;
-			      y = event.pageY - drop.height() / 2 - $(this).offset().top;
-			      drop.css({
-			        top: y,
-			        left: x
-			      }).addClass("animate");
-			    }
-			  });
-
-			  $(document.body).on('dragstart', '.materialBtn, .select li', function(e) {
-			    e.preventDefault();
-			  })
+			  	//opening animation
+			  	$('.select'+scope.currentid+' li').click(function(event) {
+			  		console.info("called opening animation " + scope.currentid);
+			  	  	if (parseFloat($(this).css('opacity')) > 0 && $(document).width() >= 1008) {
+			  	    	var maxWidthHeight = Math.max($(this).width(), $(this).height());
+			  	    	if ($(this).find("b.drop").length == 0 || $(this).find("b.drop").css('opacity') != 1) {
+			  	      		drop = $('<b class="drop" style="width:' + maxWidthHeight + 'px;height:' + maxWidthHeight + 'px;"></b>').prependTo(this);
+			  	    	} else {
+			  	      		$(this).find("b.drop").each(function() {
+			  	        	if ($(this).css('opacity') == 1) {
+			  	          		drop = $(this).removeClass("animate");
+			  	          			return;
+			  	        		}
+			  	      		})
+			  	    	}
+			  	    	x = event.pageX - drop.width() / 2 - $(this).offset().left;
+			  	    	y = event.pageY - drop.height() / 2 - $(this).offset().top;
+			  	    	drop.css({top: y,left: x}).addClass("animate");
+			  	  	}
+			  	});
+	
+			  	$(document.body).on('dragstart', '.select'+scope.currentid+' li', function(e) {
+			  	  	e.preventDefault();
+			  	})
 			
-			  var selectTimeout;
-			  $(document.body).on('click', '.select li', function() {
-			    var parent = $(this).parent();
-			    parent.children('li').removeAttr('data-selected');
-			    $(this).attr('data-selected', 'true');
-			    clearTimeout(selectTimeout);
-			    if (parent.hasClass('isOpen')) {
-			      if (parent.parent().hasClass('required')) {
-			        if (parent.children('[data-selected]').attr('data-value')) {
-			          parent.parents('.materialSelect').removeClass('error empty');
-			        } else {
-			          parent.parents('.materialSelect').addClass('error empty');
-			        }
-			      }
-			      hideMaterialList($('.select'));
-			    } else {
-			      var pos = Math.max(($('li[data-selected]', parent).index() - 2) * 48, 0);
-			      parent.addClass('isOpen');
-			      parent.parent().css('z-index', '999');
-			      if ($(document).width() >= 1008) {
-			        var i = 1;
-			        selectTimeout = setInterval(function() {
-			          i++; 
-			          if (i == 2) {
-			            parent.css('overflow', 'auto');
-			          }
-			          if (i >= 4) {
-			            clearTimeout(selectTimeout);
-			          }
-			        }, 100);
-			      }
-			    }
-			  });
+			  	//select a choice exit
+			  	var selectTimeout;
+			  	$(document.body).on('click', '.select'+scope.currentid+' li', function() {
+			  	  	var parent = $(this).parent();
+			  	  	parent.children('li').removeAttr('data-selected');
+			  	  	$(this).attr('data-selected', 'true');
+			  	  	clearTimeout(selectTimeout);
+			  	  	if (parent.hasClass('isOpen')) {
+			  	  	  	hideMaterialList($('.select'+scope.currentid));
+			  	  	} else {
+			  	  	  	var pos = Math.max(($('li[data-selected]', parent).index() - 2) * 48, 0);
+			  	  	  	parent.addClass('isOpen');
+			  	  	  	parent.parent().css('z-index', '999');
+			  	  	  	if ($(document).width() >= 1008) {
+			  	  	    	var i = 1;
+			  	  	    	selectTimeout = setInterval(function() { i++; 
+			  	  	      		if (i == 2) { parent.css('overflow', 'auto'); }
+			  	  	      		if (i >= 4) { clearTimeout(selectTimeout); }
+			  	  	    	}, 100);
+			  	  	  	}
+			  	  	}
+			  	});
 			
-			  $('.materialInput input').on('change input verify', function() {
-			    if ($(this).attr('required') == 'true') {
-			      if ($(this).val().trim().length) {
-			        $(this).parent().removeClass('error empty');
-			      } else {
-			        $(this).parent().addClass('error empty');
-			        $(this).val('');
-			      }
-			    } else {
-			      if ($(this).val().trim().length) {
-			        $(this).parent().removeClass('empty');
-			      } else {
-			        $(this).parent().addClass('empty');
-			      }
-			    }
-			  });
-			
-			  $(document.body).on('click', function(e) {
-			    var clicked;
-			    if ($(e.target).hasClass('materialSelect')) {
-			      clicked = $(e.target).find('.select').first();
-			    } else if ($(e.target).hasClass('select')) {
-			      clicked = $(e.target);
-			    } else if ($(e.target).parent().hasClass('select')) {
-			      clicked = $(e.target).parent();
-			    }
-			
-			    if ($(e.target).hasClass('materialSelect') || $(e.target).hasClass('select') || $(e.target).parent().hasClass('select')) {
-			      hideMaterialList($('.select').not(clicked));
-			    } else {
-			      if ($('.select').hasClass('isOpen')) {
-			        hideMaterialList($('.select'));
-			      }
-			    }
-			  });
-			  hideMaterialList($('.select'));
+			  	//click on body exit
+			  	$(document.body).on('click', function(e) {
+			  	  	var clicked;
+			  	  	if ($(e.target).hasClass('materialSingleSelect')) {
+			  	  	  	clicked = $(e.target).find('.select'+scope.currentid).first();
+			  	  	} else if ($(e.target).hasClass('select'+scope.currentid)) {
+			  	  	  	clicked = $(e.target);
+			  	  	} else if ($(e.target).parent().hasClass('select'+scope.currentid)) {
+			  	  	  	clicked = $(e.target).parent();
+			  	  	}
+					
+			  	  	if ($(e.target).hasClass('materialSingleSelect') || 
+			  	  		$(e.target).hasClass('select'+scope.currentid) || 
+			  	  		$(e.target).parent().hasClass('select'+scope.currentid)) {
+			  	  	  	hideMaterialList($('.select'+scope.currentid).not(clicked));
+			  	  	} else {
+			  	  	  	if ($('.select').hasClass('isOpen')) {
+			  	  	    	hideMaterialList($('.select'+scope.currentid));
+			  	  	  	}
+			  	  	}
+			  	});
+			  	hideMaterialList($('.select'+scope.currentid));
 			})
 
 		}
